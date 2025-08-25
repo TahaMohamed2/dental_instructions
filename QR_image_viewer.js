029c45627338c0dfeeaa3b9548b03434fb63a87e
Taha Mohamed Taha <dr.taha.1998@gmail.com>
2025-08-25 12:17:14 +0300

    pop qr for all pages

--- QR_image_viewer.js
+++ QR_image_viewer.js
@@ -8,7 +8,7 @@ function viewQRImage() {
 
     // Create a new img element for the QR image
     const qrImage = document.createElement('img');
-    qrImage.src = 'resources/QR.png';
+    qrImage.src = 'resources/QR.jpeg';
     qrImage.alt = 'QR';
 
     // Add an event listener for the image load event
